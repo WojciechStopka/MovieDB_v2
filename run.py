b34c5dba4643c69a72ecb781465a317a877e20b2
Wojciech Stopka <w.stopka95@gmail.com>
2023-07-06 11:25:36 +0200

    requirements.txt added to the project

--- run.py
+++ run.py
@@ -2,9 +2,9 @@ from client import MoviesClient
 from visualisation import popular_movies_visualisation, rated_movies_visualisation
 
 if __name__ == "__main__":
-	new_client = MoviesClient()
-	rated_movies = new_client.get_rated_by_user_movies()
-	popular_movies = new_client.get_popular_movies(15)
-	average_score = new_client.get_average_user_rating()
-	rated_movies_visualisation(rated_movies, average_score)
-	popular_movies_visualisation(popular_movies)
+    new_client = MoviesClient()
+    rated_movies = new_client.get_rated_by_user_movies()
+    popular_movies = new_client.get_popular_movies()
+    average_score = new_client.get_average_user_rating()
+    rated_movies_visualisation(rated_movies, average_score)
+    popular_movies_visualisation(popular_movies)
